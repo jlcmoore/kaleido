4d78ac0e4859f0611648c9612f18b0fb111c81b1
Jared Moore <jared@jaredmoore.org>
2024-02-03 08:52:31 -0800

    make it a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("requirements.txt", "r") as file:
+    requirements = file.read().splitlines()
+
+setuptools.setup(
+    name="kaleido",
+    version="0.0.1",
+    author="Taylor Sorensen",
+    author_email="tsor13@cs.washington.edu",
+    description="Value Kaleidoscope",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements,
+)
